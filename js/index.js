d601767f53e747a1bc536721ee334aff280676ff
rar9000 <clavius@Clavius.local>
2025-01-09 15:11:24 -0500

    first part done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,49 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+let healthPoints = 4;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
+
+let dayTime = false;
+
+if (dayTime === false) {
+    console.log("It is so dark");
+} else {
+    console.log("Sunshine!");
+}
+
+let sick = true;
+
+if (sick = true) {
+    console.log("I'm gonna blow!");
+} else {
+    console.log("Healthy as a horse");
+}
+
+let bodyTemp = 58
+
+if (bodyTemp >= 97 && bodyTemp <=98 ) {
+    console.log("Healthy as a horse");
+} else if (bodyTemp === 99) {
+    console.log("Sorta sick");
+} else if (bodyTemp > 99 && bodyTemp <= 101) {
+    console.log("Sick as a dog");
+} else if (bodyTemp > 101 && bodyTemp <= 105) {
+    console.log("Call an ambulance!");
+} else if (bodyTemp > 105) {
+    console.log("You're on fire!");
+} else if (bodyTemp >= 95 && bodyTemp <= 96) {
+    console.log("Sip a hot soup"); 
+} else {
+    console.log("Congrats, you're died!");
+}
+*/
